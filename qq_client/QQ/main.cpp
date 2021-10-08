f9f0053d8d7d11a375c85228c8fa0ee4ed2aabf0
MonotagariL <73738864+MonogatariL@users.noreply.github.com>
2021-10-08 08:24:45 +0800

    Update main.cpp

--- qq_client/QQ/main.cpp
+++ qq_client/QQ/main.cpp
@@ -6,11 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     qq_main_menu w;
-    internet client;
-    qq_list q(&client,"1060483009","1146011");
-    chat c(&client,"123","123","123");
-    c.show();
-    q.show();
     w.show();
     return a.exec();
 }
